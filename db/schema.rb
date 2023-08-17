5872b507f51371e59526a6a57a91936c934bc5cb
Lilia Bruffaerts <bruffaert@icloud.com>
2023-08-17 17:46:23 +0200

    tasks finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_122037) do
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.integer "rating"
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_123225) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
